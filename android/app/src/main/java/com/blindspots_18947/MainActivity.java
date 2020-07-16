6f18b30a5e4c025f7b5577a3cbba469b71e4f71d
Crowdbotics <team@crowdbotics.com>
2020-07-16 19:05:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/blindspots_18947/MainActivity.java
@@ -0,0 +1,15 @@
+package com.blindspots_18947;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "blindspots_18947";
+    }
+}
